f82ba4ea5b73d4902911fa9ac7957489ddadce95
風一樣的男子 <xiegengcai@foxmail.com>
2016-09-21 09:06:05 +0800

    修改RpcService模板

--- src/main/resources/template/java/${basepackage_dir}/${subpackage}/service/rpc/${className}RpcServiceImpl.java
+++ src/main/resources/template/java/${basepackage_dir}/${subpackage}/service/rpc/${className}RpcServiceImpl.java
@@ -10,7 +10,7 @@ import ${basepackage}.${subpackage}.service.${className}Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 <#include "java_imports.include">
-@RpcService("${classNameLower}Serivce")
+@RpcService("${classNameLower}RpcSerivce")
 public class ${className}RpcServiceImpl extends BaseRpcService<${className}, ${className}Service> implements ${className}RpcService{
     @Autowired
     private ${className}Service ${classNameLower}Service;
