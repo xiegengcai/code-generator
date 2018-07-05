33954395d36338a66c140fc6f674c2e3d045d051
xiegengcai <xiegengcai@foxmail.com>
2018-07-05 23:01:38 +0800

    更新模版

---
+++ src/main/resources/template/java/${basepackage_dir}/${subpackage}/controller/${className}Controller.java
@@ -0,0 +1,50 @@
+<#include "java_copyright.include">
+<#assign className = table.className>   
+<#assign classNameLower = className?uncap_first>   
+package ${basepackage}.${subpackage}.controller;
+
+import cn.leta.common.utils.BeanUtil;
+import cn.leta.common.vo.LetaPage;
+import cn.leta.common.web.BaseController;
+import ${basepackage}.${subpackage}.model.entity.${className};
+import ${basepackage}.${subpackage}.service.${className}Service;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+<#include "java_imports.include">
+@Api(tags = "${table.comment}操作接口")
+@RestController
+public SysDepartmentController  ${className}Controller extends BaseController<${className}, ${className}Service>{
+	
+	@ApiOperation("按ID查询${table.comment}")
+    @GetMapping("/${classNameLower}/{id}")
+    public ${className} get(@PathVariable @ApiParam(value = "ID", required = true) int id) {
+        return baseService.selectById(id);
+    }
+
+    @ApiOperation("增加${table.comment}")
+    @PostMapping("/${classNameLower}")
+    public void add(@RequestBody @Valid ${className} ${classNameLower}) {
+        baseService.insert(${classNameLower});
+    }
+
+    @ApiOperation("按ID删除${table.comment}")
+    @DeleteMapping("/${classNameLower}/{id}")
+    public void delete(@PathVariable @ApiParam(value = "ID", required = true) int id) {
+        baseService.deleteById(id);
+    }
+
+    @ApiOperation("更新${table.comment}")
+    @PutMapping("/${classNameLower}")
+    public void edit(@RequestBody  @Valid ${className} ${classNameLower}) {
+        baseService.updateById(${classNameLower});
+    }
+    @ApiOperation("分页查询")
+    @GetMapping("/${classNameLower}/query")
+    public LetaPage<${className}> query(@RequestBody LetaPage<${className}> letaPage) {
+        return BeanUtil.toLetaPage(baseService.selectPage(letaPage.toPage()));
+    }
+}
